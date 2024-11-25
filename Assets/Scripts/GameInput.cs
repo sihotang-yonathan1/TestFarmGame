bd7d71a237d817c601657f3837ee4853c272dc00
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-11-25 16:39:06 +0800

    [input] Refactor arrow key to separate category

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,14 +1,18 @@
+using System;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    public EventHandler OnInteractAction;
     private PlayerInputAction inputActions;
 
     private void Awake()
     {
         inputActions = new PlayerInputAction();
         inputActions.Player.Enable();
+
     }
+
     public Vector2 GetMovementVectorNormalized()
     {
         Vector2 result = inputActions.Player.Move.ReadValue<Vector2>();
